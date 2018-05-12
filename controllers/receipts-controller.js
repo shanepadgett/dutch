bb86e2ba964fc8d872ae33c94d7dbd4eade901cb
Shane Padgett <spadgett00@gmail.com>
2018-05-12 00:33:38 -0500

    Mock receipt data for routes

---
+++ controllers/receipts-controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'getReceipts': require('../endpoints/receipts/get-receipts'),
+  'getReceipt': require('../endpoints/receipts/get-receipt'),
+  'postReceipt': require('../endpoints/receipts/post-receipt')
+}
