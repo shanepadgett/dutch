1897da99df4dddb7b7302fa959aab67524f4e911
Shane Padgett <spadgett00@gmail.com>
2018-05-24 13:30:04 -0500

    clicking pay sets receipt items to paid

--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -2,5 +2,5 @@ const {createItem, updateItem} = require('../controllers/item-controller')
 
 module.exports = app => {
   app.post('/api/items/', createItem)
-  app.put('/api/items/:id', updateItem)
+  app.post('/api/items/:id', updateItem)
 }
